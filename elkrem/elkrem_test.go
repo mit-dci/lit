1ac03ec9caca7d9b0678ff3a5d811f0fa498767d
adiabat <rx@awsomnet.org>
2016-12-03 16:45:38 -0500

    add test cases to elkrem

--- elkrem/elkrem_test.go
+++ elkrem/elkrem_test.go
@@ -69,3 +69,33 @@ func TestElkremLess(t *testing.T) {
 			n, sha.String())
 	}
 }
+
+func TestFixed(t *testing.T) {
+	root, _ := chainhash.NewHashFromStr(
+		"b43614f251760d689adf84211148a40d7dee13967b7109e13c8d1437a4966d58")
+
+	sndr := NewElkremSender(*root)
+
+	zero, _ := chainhash.NewHashFromStr(
+		"2a124935e0713149b71ff17cb43465e9828bacd1e833f0dc08460783a6a42cb4")
+
+	thousand, _ := chainhash.NewHashFromStr(
+		"0151a39169940cdd8ccf1ba619f254ddbf16ce260a243528839b2634eaa63d0a")
+
+	for n := uint64(0); n < 5000; n += 500 {
+		sha, err := sndr.AtIndex(n)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("elk %d: %s\n", n, sha.String())
+
+		if n == 0 && !sha.IsEqual(zero) {
+			t.Fatalf("Elk %d expected %s, got %s", n, zero.String(), sha.String())
+		}
+		if n == 1000 && !sha.IsEqual(thousand) {
+			t.Fatalf("Elk %d expected %s, got %s", n, zero.String(), sha.String())
+		}
+
+	}
+
+}
