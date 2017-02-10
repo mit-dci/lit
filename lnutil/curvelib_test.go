7a37afdc9576d307f6772d906c55ec95a986c0d0
Takaya Imai <takaya.imai@frontier-ptnrs.com>
2017-02-11 00:16:54 +0900

    add unit-test for curvelib.go

---
+++ lnutil/curvelib_test.go
@@ -0,0 +1,29 @@
+package lnutil
+
+import (
+	"testing"
+)
+
+// AddPubsEZ
+func TestAddPubsEZ(t *testing.T) {
+}
+
+// CombinePubs
+func TestCombinePubs(t *testing.T) {
+}
+
+// CombinePrivKeyWithBytes
+func TestCombinePrivKeyWithBytes(t *testing.T) {
+}
+
+// CombinePrivKeyAndSubtract
+func TestCombinePrivKeyAndSubtract(t *testing.T) {
+}
+
+// ElkScalar
+func TestElkScalar(t *testing.T) {
+}
+
+// ElkPointFromHash
+func TestElkPointFromHash(t *testing.T) {
+}
