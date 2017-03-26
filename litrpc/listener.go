b330904d8bb1f0a3d48965a60404755bf09213ed
adiabat <rx@awsomnet.org>
2017-03-26 19:29:49 -0400

    move from json-rpc over TCP to json-rpc over HTTP
    it's ugly but everyone loves the web

--- litrpc/listener.go
+++ litrpc/listener.go
@@ -2,8 +2,10 @@ package litrpc
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"net/http"
 	"net/rpc"
 	"net/rpc/jsonrpc"
 	"time"
@@ -21,9 +23,38 @@ It ends up being the root of ~everything in the executable.
 
 // A LitRPC is the user I/O interface; it owns and initialized a SPVCon and LitNode
 // and listens and responds on RPC
+
+var GlobalServer *rpc.Server
+
 type LitRPC struct {
 	Node      *qln.LitNode
 	OffButton chan bool
+}
+
+type HttpConn struct {
+	in  io.Reader
+	out io.Writer
+}
+
+func (c *HttpConn) Read(p []byte) (n int, err error)  { return c.in.Read(p) }
+func (c *HttpConn) Write(d []byte) (n int, err error) { return c.out.Write(d) }
+func (c *HttpConn) Close() error                      { return nil }
+
+func JSONRPCoverHTTPHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/lit" { // all RPC calls have path /lit
+		serverCodec :=
+			jsonrpc.NewServerCodec(&HttpConn{in: r.Body, out: w})
+		w.Header().Set("Content-type", "application/json")
+		w.WriteHeader(200)
+		// this is super ugly; find a better way to do this.
+		// maybe try a different struct with a ServeHTTP method.
+		err := GlobalServer.ServeRequest(serverCodec)
+		if err != nil {
+			log.Printf("JSON-RPC request serving error: %v", err)
+			http.Error(w, "JSON-RPC request error", 500)
+			return
+		}
+	}
 }
 
 func RpcListen(node *qln.LitNode, port uint16) {
@@ -31,9 +62,10 @@ func RpcListen(node *qln.LitNode, port uint16) {
 	rpcl.Node = node
 	rpcl.OffButton = make(chan bool, 1)
 
-	server := rpc.NewServer()
-	server.Register(rpcl)
-	server.HandleHTTP(rpc.DefaultRPCPath, rpc.DefaultDebugPath)
+	GlobalServer = rpc.NewServer()
+	GlobalServer.Register(rpcl)
+
+	//	server.HandleHTTP(rpc.DefaultRPCPath, rpc.DefaultDebugPath)
 
 	portString := fmt.Sprintf(":%d", port)
 	listener, err := net.Listen("tcp", portString)
@@ -41,18 +73,24 @@ func RpcListen(node *qln.LitNode, port uint16) {
 		fmt.Printf(err.Error())
 		return
 	}
+	defer listener.Close()
 
-	go func() {
-		for {
-			conn, err := listener.Accept()
-			if err != nil {
-				log.Printf("listener error: " + err.Error())
-			} else {
-				log.Printf("new connection from %s\n", conn.RemoteAddr().String())
-				go server.ServeCodec(jsonrpc.NewServerCodec(conn))
+	http.Serve(listener, http.HandlerFunc(JSONRPCoverHTTPHandler))
+
+	/*
+		go func() {
+			for {
+				conn, err := listener.Accept()
+				if err != nil {
+					log.Printf("listener error: " + err.Error())
+				} else {
+					log.Printf("new connection from %s\n", conn.RemoteAddr().String())
+					go server.ServeCodec(jsonrpc.NewServerCodec(conn))
+				}
 			}
-		}
-	}()
+		}()
+
+	*/
 
 	// ugly; add real synchronization here
 	<-rpcl.OffButton
