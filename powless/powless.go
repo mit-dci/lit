89b22ce1d17fa6332ddcd01dc2762344633634e9
adiabat <rx@awsomnet.org>
2017-04-13 15:53:51 -0400

    Capitalize fields for json.

--- powless/powless.go
+++ powless/powless.go
@@ -2,15 +2,18 @@ package powless
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net"
 	"net/http"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/wire"
+	"github.com/btcsuite/btcutil"
 	"github.com/mit-dci/lit/lnutil"
 )
 
@@ -49,6 +52,11 @@ type APILink struct {
 	TxUpToWallit chan lnutil.TxAndHeight
 
 	CurrentHeightChan chan int32
+
+	// time based polling
+	dirtybool bool
+
+	p *chaincfg.Params
 }
 
 func (a *APILink) Start(
@@ -56,6 +64,7 @@ func (a *APILink) Start(
 	chan lnutil.TxAndHeight, chan int32, error) {
 
 	// later, use params to detect which api to connect to
+	a.p = params
 
 	a.TrackingAdrs = make(map[[20]byte]bool)
 	a.TrackingOPs = make(map[wire.OutPoint]bool)
@@ -63,13 +72,34 @@ func (a *APILink) Start(
 	a.TxUpToWallit = make(chan lnutil.TxAndHeight, 1)
 	a.CurrentHeightChan = make(chan int32, 1)
 
+	go a.ClockLoop()
+
 	return a.TxUpToWallit, a.CurrentHeightChan, nil
+}
+
+func (a *APILink) ClockLoop() {
+
+	for {
+		if a.dirtybool {
+			a.dirtybool = false
+			err := a.GetAdrTxos()
+			if err != nil {
+				fmt.Printf(err.Error())
+			}
+		} else {
+			fmt.Printf("clean, sleep 5 sec\n")
+			time.Sleep(time.Second * 5)
+		}
+	}
+
+	return
 }
 
 func (a *APILink) RegisterAddress(adr160 [20]byte) error {
 	a.TrackingAdrsMtx.Lock()
 	a.TrackingAdrs[adr160] = true
 	a.TrackingAdrsMtx.Unlock()
+	a.dirtybool = true
 	return nil
 }
 
@@ -77,6 +107,84 @@ func (a *APILink) RegisterOutPoint(op wire.OutPoint) error {
 	a.TrackingOPsMtx.Lock()
 	a.TrackingOPs[op] = true
 	a.TrackingOPsMtx.Unlock()
+	a.dirtybool = true
+	return nil
+}
+
+// ARGHGH all fields have to be exported (caps) or the json unmarshaller won't
+// populate them !
+type AdrResponse struct {
+	Success bool
+	//	Paging  interface{}
+	Unspent []jsutxo
+}
+
+type jsutxo struct {
+	Value_int int64
+	Txid      string
+	N         uint32
+	Addresses []string // why more than 1 ..?
+}
+
+func (a *APILink) GetAdrTxos() error {
+	apiurl := "https://testnet-api.smartbit.com.au/v1/blockchain/address/"
+	// make a comma-separated list of base58 addresses
+	var adrlist string
+
+	a.TrackingAdrsMtx.Lock()
+	for adr160, _ := range a.TrackingAdrs {
+		adr58, err := btcutil.NewAddressPubKeyHash(adr160[:], a.p)
+		if err != nil {
+			return err
+		}
+		adrlist += adr58.String()
+		adrlist += ","
+	}
+
+	// chop off last comma
+	adrlist = adrlist[:len(adrlist)-1] + "/unspent"
+
+	response, err := http.Get(apiurl + adrlist)
+	if err != nil {
+		return err
+	}
+
+	ar := new(AdrResponse)
+
+	err = json.NewDecoder(response.Body).Decode(ar)
+	if err != nil {
+		return err
+	}
+
+	if !ar.Success {
+		return fmt.Errorf("success = false...")
+	}
+	//	if len(ar.unspent) == 0 {
+	//		fmt.Printf("didn't work")
+	//	}
+	/*
+		dec := json.NewDecoder(response.Body)
+		err = dec.Decode(ar)
+		if err != nil {
+			return err
+		}
+		if dec.More() {
+			return fmt.Errorf("there's still more")
+		}
+		if !ar.success {
+			return fmt.Errorf("success = false...")
+		}
+		if len(ar.unspent) == 0 {
+			fmt.Printf("didn't work")
+		}
+	*/
+
+	fmt.Printf("got a txid %s\n", ar.Unspent[0].Txid)
+
+	//	fmt.Printf("\nresponse : sat: %d outpoint: %s;%d\n", ar.value_int, ar.txid, ar.n)
+
+	//	_, err = io.Copy(os.Stdout, response.Body)
+
 	return nil
 }
 
