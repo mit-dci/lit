e5c0eafa131c52be5dcc72edcb929950acee4858
joseph <jsphkn@github.com>
2017-05-02 20:45:42 -0400

    half-working

--- cmd/litpy/litrpc.py
+++ cmd/litpy/litrpc.py
@@ -6,6 +6,13 @@ import sys
 import requests
 import random
 import subprocess
+import os
+import signal
+import shutil
+import threading
+from string import ascii_lowercase, digits
+import random
+import time
 
 class RegtestConn:
 	rpcuser = "regtestuser"
@@ -126,24 +133,65 @@ class LitConn:
 		resp = json.loads(self.ws.recv())
 		#TODO: get different kinds of balances
 		return resp["result"]["TxoTotal"]
+"""
+class litThread(threading.Thread):
+	def __init__(self, threadID, name):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		
+	def run(self):
+		print ("Starting " + self.name)
+		#subprocess.call(["./../../lit","-spv","127.0.0.1","-tip","1080000","-reg", "-dir", "/dev/shm/test1"])
+		# The os.setsid() is passed in the argument preexec_fn so
+		# it's run after the fork() and before  exec() to run the shell.
+		pro = subprocess.Popen(["./../../lit","-spv","127.0.0.1","-tip","1080000","-reg", "-dir", "/dev/shm/test1"], shell=True)
+		return pro
+		
+	def kill(self):
+		os.killpg(os.getpgid(self.pro.pid), signal.SIGTERM)  # Send the signal to all the process groups
+		print ("Exiting " + self.name)
+		"""
 
 def main(args):
-	#subprocess.call(["bitcoind","-daemon","-regtest"])
-	#subprocess.call(["./../../../lit","-spv","127.0.0.1","-tip","1080000","-reg"])
-
-	newConn = RegtestConn()
-	newConn.mineblock(101)
-	newConn.getinfo()
-
-	litConn = LitConn()
-	bal = litConn.getBal()
-	print(bal)
-	addr = litConn.getLegacyAddress()
-	print(addr)
-	newConn.sendTo(addr, 10.00)
-	resp0 = litConn.litSend([addr], [1000000])
-	print(resp0)
-
+	
+	subprocess.call(["bitcoind","-daemon","-regtest"])
+	if os.path.exists("/dev/shm/test1"):
+		shutil.rmtree("/dev/shm/test1")
+	os.makedirs("/dev/shm/test1")
+	f = open("/dev/shm/test1/testkey.hex","w+")
+	#randomString = os.urandom(32)
+	randomString = ''.join(random.choice("abcdef"+digits) for i in range(64))
+	f.write(randomString + "\n")
+	f.close()
+	
+	litProcess = subprocess.Popen(["./../../lit","-spv","127.0.0.1","-reg", "-dir", "/dev/shm/test1","-v"], shell=True)
+	time.sleep(3)
+	
+	
+	try:
+		newConn = RegtestConn()
+		newConn.mineblock(1)
+		newConn.getinfo()
+	
+		litConn = LitConn()
+		bal = litConn.getBal()
+		print(bal)
+		addr = litConn.getLegacyAddress()
+		print(addr)
+		newConn.sendTo(addr, 12.34)
+		#addr = litConn.getWitAddress()
+		#resp0 = litConn.litSend([addr], [1000000])
+		#print(resp0)
+		
+		litProcess.terminate()
+	except:
+		print("error!!!")
+		print(sys.exc_info())
+		litProcess.terminate()
+		
+	
 if __name__ == '__main__':
-    main(sys.argv)
+	main(sys.argv)
+		
     
