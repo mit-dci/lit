f54efad7253a66566379853edd7c1f7e70ba0965
John Newbery <john@johnnewbery.com>
2017-05-05 10:24:16 -0400

    [tests] test Listen and Connect
    This commit updates test_lit to start two lit nodes, then listens for a
    connection on node0 and connects from node1.

--- cmd/litpy/test_lit.py
+++ cmd/litpy/test_lit.py
@@ -26,21 +26,48 @@ class LitNode():
 
 
 def testLit():
-    """starts a lit process and tests basic functionality:
+    """starts two lit processes and tests basic functionality:
 
     - connect over websocket
     - create new address
     - get balance
+    - listen on node0
+    - connect from node1 to node0
     - stop"""
 
-    node = LitNode(1)
-    node.start_node()
+    # Start lit node 0 and open websocket connection
+    node0 = LitNode(0)
+    node0.start_node()
 
-    litConn = litrpc.LitConnection("127.0.0.1", "8001")
-    litConn.connect()
-    litConn.new_address()
-    litConn.Bal()
-    litConn.Stop()
+    litConn0 = litrpc.LitConnection("127.0.0.1", "8001")
+    litConn0.connect()
+    litConn0.new_address()
+    litConn0.Bal()
+
+    # Start lit node 1 and open websocket connection
+    node1 = LitNode(1)
+    node1.args.extend(["-rpcport", "8002"])
+    node1.start_node()
+
+    litConn1 = litrpc.LitConnection("127.0.0.1", "8002")
+    litConn1.connect()
+    litConn1.new_address()
+    litConn1.Bal()
+
+    # Listen on lit node0 and connect from lit node1
+    res = litConn0.Listen(Port="127.0.0.1:10001")["result"]
+    node0.lit_address = res["Status"].split(' ')[5] + '@' + res["Status"].split(' ')[2]
+
+    res = litConn1.Connect(LNAddr=node0.lit_address)
+    assert not res['error']
+
+    # Check that node0 and node1 are connected
+    assert len(litConn0.ListConnections()['result']['Connections']) == 1
+    assert len(litConn1.ListConnections()['result']['Connections']) == 1
+
+    # Stop lit nodes
+    litConn0.Stop()
+    litConn1.Stop()
 
     print("Test succeeds!")
 
