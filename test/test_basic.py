e66655e6bc2415fb2003a84580082d935432c3b6
John Newbery <john@johnnewbery.com>
2017-05-16 17:42:37 -0400

    [tests] refactor test_basic to be like test_lc

--- test/test_basic.py
+++ test/test_basic.py
@@ -4,34 +4,33 @@
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 """Test basic lit functionality
 
-- start bitcoind process
-- start two lit processes
+- start coin node
+- start two lit nodes
 - connect over websocket
 - create new address
 - get balance
-- listen on litnode0
-- connect from litnode1 to litnode0
-- send funds from bitcoind process to litnode0 address
-- open channel between litnode0 and litnode1
-- push funds from litnode0 to litnode1
+- listen on lit node 0
+- connect from lit node 1 to lit node 0
+- send funds from coin node to lit node 0 address
+- open channel between lit node 0 and lit node 1
+- push funds from lit node 0 to lit node 1
 - push funds back
 - close channel co-operatively
 - stop"""
-import time
-
 from lit_test_framework import LitTest, wait_until
 
 BC_REGTEST = 257
 
-class TestBasic(LitTest):
+class TestLC(LitTest):
     def run_test(self):
 
-        # Start a bitcoind node
+        # Start a coin node
         self.add_bcnode()
         self.bcnodes[0].start_node()
 
         self.log.info("Generate 500 blocks to activate segwit")
-        self.bcnodes[0].generate(nblocks=500)
+        self.bcnodes[0].generate(500)
+        self.chain_height = 500
         network_info = self.bcnodes[0].getblockchaininfo().json()['result']
         assert network_info['bip9_softforks']['segwit']['status'] == 'active'
 
@@ -53,37 +52,34 @@ class TestBasic(LitTest):
 
         res = self.litnodes[1].Connect(LNAddr=self.litnodes[0].lit_address)
         assert not res['error']
-        time.sleep(1)
 
         # Check that litnode0 and litnode1 are connected
-        assert len(self.litnodes[0].ListConnections()['result']['Connections']) == 1
+        wait_until(lambda: len(self.litnodes[0].ListConnections()['result']['Connections']) == 1)
         assert len(self.litnodes[1].ListConnections()['result']['Connections']) == 1
         self.log.info("lit nodes connected")
 
-        self.log.info("Send funds from bitcoind node to litnode0")
+        self.log.info("Send funds from coin node to lit node 0")
         balance = self.litnodes[0].get_balance(BC_REGTEST)
         self.log_balances(BC_REGTEST)
-        addr = self.litnodes[0].rpc.new_address()
-        self.bcnodes[0].sendtoaddress(address=addr["result"]["LegacyAddresses"][0], amount=12.34)
-        self.bcnodes[0].generate(nblocks=1).text
+        addr = self.litnodes[0].rpc.Address(NumToMake=1, CoinType=BC_REGTEST)
+        self.bcnodes[0].sendtoaddress(addr["result"]["LegacyAddresses"][0], 12.34)
+        self.confirm_transactions(self.bcnodes[0], self.litnodes[0], 1)
 
         self.log.info("Waiting to receive transaction")
 
-        # Wait for transaction to be received (15 seconds timeout)
+        # Wait for transaction to be received by lit node
         wait_until(lambda: self.litnodes[0].get_balance(BC_REGTEST) - balance == 1234000000)
         balance = self.litnodes[0].get_balance(BC_REGTEST)
-        self.log.info("Funds received")
+        self.log.info("Funds received by lit node 0")
         self.log_balances(BC_REGTEST)
 
-        self.log.info("Send money from litnode0 to its own segwit address and confirm")
+        self.log.info("Send money from lit node 0 to its own segwit address and confirm")
         self.log.info("sending 1000000000 satoshis to litnode1 address")
-        self.bcnodes[0].generate(nblocks=1)
         self.litnodes[0].Send(DestAddrs=[self.litnodes[0].Address()['result']['WitAddresses'][0]], Amts=[1000000000])
-        self.bcnodes[0].generate(nblocks=1)
-        wait_until(lambda: self.litnodes[0].Balance()['result']["Balances"][0]["SyncHeight"] == 503)
+        self.confirm_transactions(self.bcnodes[0], self.litnodes[0], 1)
 
-        # We'll lose some money to fees. Hopefully not too much!
-        assert balance - self.litnodes[0].get_balance(BC_REGTEST) < 50000
+        # We'll lose some money to fees.
+        assert balance - self.litnodes[0].get_balance(BC_REGTEST) < 200000
         balance = self.litnodes[0].get_balance(BC_REGTEST)
         self.log.info("Funds transferred to segwit address")
         self.log_balances(BC_REGTEST)
@@ -92,15 +88,16 @@ class TestBasic(LitTest):
         assert self.litnodes[0].ChannelList()['result']['Channels'] == []
         assert self.litnodes[1].ChannelList()['result']['Channels'] == []
 
-        self.litnodes[0].FundChannel(Peer=1, CoinType=257, Capacity=1000000000)
-        self.log.info("litnode0 has funded channel")
+        self.litnodes[0].FundChannel(Peer=1, CoinType=BC_REGTEST, Capacity=1000000000)
+        self.confirm_transactions(self.bcnodes[0], self.litnodes[0], 1)
+        self.log.info("lit node 0 has funded channel")
 
         # Wait for channel to open
         wait_until(lambda: len(self.litnodes[0].ChannelList()['result']['Channels']) > 0)
         assert len(self.litnodes[1].ChannelList()['result']['Channels']) > 0
         self.log.info("Channel open")
 
-        assert abs(balance - self.litnodes[0].get_balance(BC_REGTEST) - 1000000000) < 50000
+        assert abs(balance - self.litnodes[0].get_balance(BC_REGTEST) - 1000000000) < 200000
         balance = self.litnodes[0].get_balance(BC_REGTEST)
         self.log_balances(BC_REGTEST)
 
@@ -118,7 +115,7 @@ class TestBasic(LitTest):
         assert litnode1_channel['MyBalance'] == 0
 
         self.log_channel_balance(self.litnodes[0], 0, self.litnodes[1], 0)
-        self.log.info("Now push some funds from litnode0 to litnode1")
+        self.log.info("Now push some funds from lit node 0 to lit node 1")
 
         self.litnodes[0].Push(ChanIdx=1, Amt=100000000)
 
@@ -138,15 +135,15 @@ class TestBasic(LitTest):
         assert litnode1_channel['MyBalance'] == 50000000
 
         self.log_channel_balance(self.litnodes[0], 0, self.litnodes[1], 0)
+
         self.log.info("Close channel")
         self.litnodes[0].CloseChannel(ChanIdx=1)
-        self.bcnodes[0].generate(nblocks=10)
-        wait_until(lambda: self.litnodes[0].Balance()['result']["Balances"][0]["SyncHeight"] == 513)
+        self.confirm_transactions(self.bcnodes[0], self.litnodes[0], 1)
 
-        assert abs(self.litnodes[1].get_balance(BC_REGTEST) - 50000000) < 50000
-        assert abs(balance + 950000000 - self.litnodes[0].get_balance(BC_REGTEST)) < 50000
+        wait_until(lambda: abs(self.litnodes[1].get_balance(BC_REGTEST) - 50000000) < 200000)
+        assert abs(balance + 950000000 - self.litnodes[0].get_balance(BC_REGTEST)) < 200000
 
         self.log_balances(BC_REGTEST)
 
 if __name__ == "__main__":
-    exit(TestBasic().main())
+    exit(TestLC().main())
