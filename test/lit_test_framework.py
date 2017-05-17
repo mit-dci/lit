f15a95aa03de3c1e6b7235db3d1ac7841b888ae3
John Newbery <john@johnnewbery.com>
2017-05-17 20:52:45 +0000

    [tests] only dump logs to console for travis

--- test/lit_test_framework.py
+++ test/lit_test_framework.py
@@ -92,7 +92,7 @@ class LitTest():
 
     def cleanup(self):
         """Cleanup test resources"""
-        if self.rc == 1:
+        if self.rc != 0 and self.args.dumplogs:
             # Dump the end of the debug logs, to aid in debugging rare
             # travis failures.
             filenames = [self.tmpdir + "/test_framework.log"]
@@ -181,6 +181,7 @@ class LitTest():
         parser = argparse.ArgumentParser(description=__doc__)
         parser.add_argument("--chains", "-c", default='reg', help="comma-separated list of coins to use for the test.")
         parser.add_argument("--debugger", "-d", action='store_true', help="Automatically attach a debugger on test failure.")
+        parser.add_argument("--dumplogs", action='store_true', help="Dump all logs to screen on failure (useful for travis failures).")
         parser.add_argument("--loglevel", "-l", default="INFO", help="log events at this level and higher to the console. Can be set to DEBUG, INFO, WARNING, ERROR or CRITICAL. Passing --loglevel DEBUG will output all logs to console. Note that logs at all levels are always written to the test_framework.log file in the temporary test directory.")
         parser.add_argument("--nocleanup", "-n", action='store_true', help="Don't clean up the test directory after running (even on success).")
         self.args, self.unknown_args = parser.parse_known_args()
