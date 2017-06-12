1b004e55ccb4460fa963c40f2fa28c73cef4a367
adiabat <rx@awsomnet.org>
2017-06-12 18:40:38 -0400

    add rpc watch call

---
+++ litrpc/watchcmds.go
@@ -0,0 +1,34 @@
+package litrpc
+
+import "fmt"
+
+// ------------------------- listen
+
+type WatchArgs struct {
+	ChanIdx uint32
+}
+
+type WatchReply struct {
+	Msg string
+}
+
+func (r *LitRPC) Watch(args WatchArgs, reply *WatchReply) error {
+
+	// load the whole channel from disk (pretty inefficient)
+	qc, err := r.Node.GetQchanByIdx(args.ChanIdx)
+	if err != nil {
+		return err
+	}
+	// see if channel is closed and error early
+	if qc.CloseData.Closed {
+		return fmt.Errorf("Can't push; channel %d closed", args.ChanIdx)
+	}
+
+	err = r.Node.SyncWatch(qc)
+	if err != nil {
+		return err
+	}
+
+	reply.Msg = "ok"
+	return nil
+}
