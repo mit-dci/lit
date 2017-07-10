44d95055f833c555409c63cdd48f663bd0d6b96c
adiabat <rx@awsomnet.org>
2017-07-10 18:15:21 -0400

    narrow down off-nominal condition

--- cmd/dlcoracle/main.go
+++ cmd/dlcoracle/main.go
@@ -1,23 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/adiabat/btcd/chaincfg/chainhash"
+	"github.com/mit-dci/lit/lnutil"
+)
 
 func main() {
 	// given an uint64, split into 2 bytes.
 	// byte1: 5 bit shift, 2 bit MSBs
 	// byte2: 7 bit LSBs
 
-	for i := uint64(1000); i < 1100; i++ {
-		ba, bb, err := split(i)
-		if err != nil {
-			panic(err)
+	/*
+		for i := uint64(1000); i < 1100; i++ {
+			ba, bb, err := split(i)
+			if err != nil {
+				panic(err)
+			}
+			//		fmt.Printf("ba:%x bb:%x\n", ba, bb)
+
+			r := join(ba, bb)
+			fmt.Printf("rejoin %d\n", r)
+
 		}
-		//		fmt.Printf("ba:%x bb:%x\n", ba, bb)
+	*/
 
-		r := join(ba, bb)
-		fmt.Printf("rejoin %d\n", r)
+	privRoot := chainhash.HashH([]byte("my private key"))
 
+	ka, Ra := deriveK(privRoot, "asset 1 a")
+
+	kb, Rb := deriveK(privRoot, "asset 1 a")
+
+	fmt.Printf("a: %x %x\n", ka, Ra)
+	fmt.Printf("b: %x %x\n", kb, Rb)
+
+	msg := chainhash.HashH([]byte("hi"))
+
+	sa, err := RSign(msg, privRoot, ka)
+	if err != nil {
+		panic(err)
 	}
+	sb, err := RSign(msg, privRoot, kb)
+	if err != nil {
+		panic(err)
+	}
+
+	pubRootArr := lnutil.PubFromHash(privRoot)
+
+	saGpub, err := SGpredict(msg, pubRootArr, Ra)
+	if err != nil {
+		panic(err)
+	}
+
+	sbGpub, err := SGpredict(msg, pubRootArr, Rb)
+	if err != nil {
+		panic(err)
+	}
+
+	saG := lnutil.PubFromHash(sa)
+	sbG := lnutil.PubFromHash(sb)
+
+	fmt.Printf("saG:\npredict\t%x\ncorrect\t%x\n", saGpub.SerializeCompressed(), saG)
+	fmt.Printf("sbG:\npredict\t%x\ncorrect\t%x\n", sbGpub.SerializeCompressed(), sbG)
 
 	return
 }
--- cmd/dlcoracle/rpoints.go
+++ cmd/dlcoracle/rpoints.go
@@ -3,8 +3,9 @@ package main
 import (
 	"crypto/hmac"
 	"crypto/sha512"
+	"math/big"
 
-	"github.com/mit-dci/lit/lnutil"
+	"github.com/adiabat/btcd/btcec"
 )
 
 // deriveK derives a k scalar from a seed and an identifier string.
@@ -12,11 +13,32 @@ import (
 // the string should be more structured but other functions can take care of that
 // derivation is just sha512 hmac with key = seed private key, data = id string
 func deriveK(seed [32]byte, id string) ([32]byte, [33]byte) {
-	var k [32]byte
-	hm := hmac.New(sha512.New, seed[:])
-	copy(k[:], hm.Sum([]byte(id)))
+	// Hardcode curve
+	curve := btcec.S256()
 
-	R := lnutil.PubFromHash(k)
+	var k [32]byte
+	var R [33]byte
+
+	Rpub := new(btcec.PublicKey)
+	Rpub.Curve = curve
+
+	hm := hmac.New(sha512.New, seed[:])
+	hm.Write([]byte(id))
+	copy(k[:], hm.Sum(nil))
+
+	bigK := new(big.Int).SetBytes(k[:])
+	// derive R = kG
+	Rpub.X, Rpub.Y = curve.ScalarBaseMult(k[:])
+
+	// If R's y-coordinate is odd, modify k.  But not R!
+	// TODO figure out why this is a thing
+	if Rpub.Y.Bit(0) == 1 {
+		bigK.Mod(bigK, curve.N)
+		bigK.Sub(curve.N, bigK)
+	}
+
+	copy(R[:], Rpub.SerializeCompressed())
+	//	R := lnutil.PubFromHash(k)
 	return k, R
 }
 
