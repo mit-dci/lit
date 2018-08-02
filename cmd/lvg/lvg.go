f128664acfee4ca0f40f2e5bf6608d3d96864fbb
Varunram <vrg2009@ymail.com>
2018-08-03 00:01:32 +0530

    nonce generation + hashing

--- cmd/lvg/lvg.go
+++ cmd/lvg/lvg.go
@@ -1,52 +1,81 @@
 package main
 
 import (
-	"encoding/hex"
-	"fmt"
+	"crypto/rand" // slows down nonce generation a lot, could use math/rand maybe?
+	"crypto/sha256"
+	//"encoding/hex"
+	//"fmt"
+	"log"
 
-	"github.com/mit-dci/lit/bech32"
-	"github.com/mit-dci/lit/btcutil"
+	//"github.com/mit-dci/lit/bech32"
+	//"github.com/mit-dci/lit/btcutil"
 	"github.com/mit-dci/lit/btcutil/btcec"
 )
 
-func GenNewAddress() (*btcec.PrivateKey, []byte, error) {
-	privk, err := btcec.NewPrivateKey(btcec.S256())
+type Hash [32]byte
+
+
+const (
+	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	letterIdxBits = 6
+	letterIdxMask = 1<<letterIdxBits - 1
+)
+
+func genNonceSecure(length int) []byte {
+
+	result := make([]byte, length)
+	bufferSize := int(float64(length) * 1.3)
+	for i, j, randomBytes := 0, 0, []byte{}; i < length; j++ {
+		if j%bufferSize == 0 {
+			randomBytes = getRandomness(bufferSize)
+		}
+		if idx := int(randomBytes[j%length] & letterIdxMask); idx < len(letterBytes) {
+			result[i] = letterBytes[idx]
+			i++
+		}
+	}
+	return result
+}
+
+// getRandomness returns the requested number of bytes using crypto/rand
+func getRandomness(length int) []byte {
+	var randomBytes = make([]byte, length)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		log.Fatal("Unable to generate random bytes")
+	}
+	return randomBytes
+}
+
+func grind(pubKey *btcec.PublicKey, nonce []byte) []byte {
+	data := make([]byte, 73) //33 + 40 bytes
+	copy(data[34:73], nonce)
+	copy(data[:33], pubKey.SerializeCompressed())
+	shasum := sha256.Sum256(data)
+	return shasum[:32]
+}
+
+func GenNewAddress() (*btcec.PublicKey, []byte, error) {
+	privKey, err := btcec.NewPrivateKey(btcec.S256())
 	if err != nil {
 		return nil, nil, err
 	}
 
-	// Run through the hashing ops.
-	hashed := btcutil.Hash160(privk.PubKey().SerializeCompressed())
+	pubKey := privKey.PubKey()
 
-	// Shorten the address (?)
-	firstNonzero := 0
-	for i, b := range hashed {
-		if b == 0 {
-			firstNonzero = i + 1
-		} else {
-			break
+	for i := 0 ; i < 600000 ; i ++ {
+		//if grind(pubKey, genNonceSecure(10)) < PoWTarget {
+		//	return pubKey, nil
+		//}
+		nonce := genNonceSecure(10)
+		a := grind(pubKey, nonce)
+		if a[0] == 0 && a[1] == 0 {
+			log.Println("NONCE FOUND")
+			log.Println(a)
+			return pubKey, nonce, nil
 		}
 	}
-	shortAddr := hashed[firstNonzero:]
-
-	return privk, shortAddr, nil
-
-}
-
-func PrintLoop(ret chan FoundAddr) {
-	for true {
-		privkey, pubkeyhash, err := GenNewAddress()
-		if err != nil {
-			panic(err)
-		}
-		if len(pubkeyhash) <= 18 {
-			ret <- FoundAddr{
-				privkey:    *privkey,
-				pubkeyhash: pubkeyhash,
-				id:         bech32.Encode("ln", pubkeyhash),
-			}
-		}
-	}
+	return pubKey, nil, nil
 }
 
 type FoundAddr struct {
@@ -56,20 +85,5 @@ type FoundAddr struct {
 }
 
 func main() {
-	ret := make(chan FoundAddr)
-	go PrintLoop(ret)
-	go PrintLoop(ret)
-	go PrintLoop(ret)
-	go PrintLoop(ret)
-	go PrintLoop(ret)
-	go PrintLoop(ret)
-	go PrintLoop(ret)
-	go PrintLoop(ret)
-
-	var next FoundAddr
-	for true {
-		next = <-ret
-		hexstr := hex.EncodeToString(next.pubkeyhash)
-		fmt.Printf("%d bytes: %s | hex: %s\n", len(next.pubkeyhash), next.id, hexstr)
-	}
+	_, _, _ = GenNewAddress()
 }
