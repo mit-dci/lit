2702ea4a139d98756e769c453413358cdb5b6649
Trey Del Bonis <j.delbonis.3@gmail.com>
2018-08-08 11:39:47 -0400

    Made a way to select which tests to run, and made sure tests can't run multiple times.

--- test/runtests.sh
+++ test/runtests.sh
@@ -3,6 +3,9 @@
 datadir=_data
 
 tests=$(cat tests.txt | grep -vE '^(#|$)')
+if [ "$#" -gt 0 ]; then
+	tests=$@
+fi
 
 echo 'Tests to run:'
 for t in $tests; do
@@ -13,12 +16,21 @@ echo '========'
 rm -rf $datadir
 
 n=0
+ign=0
 
 for t in $tests; do
 	echo "Running test: $t"
 	echo '===='
 
 	tdata=$datadir/$t
+
+	if [ -e "$tdata" ]; then
+		echo 'Directory found, was this test already run?'
+		echo '----'
+		ign=$(($ign + 1))
+		continue
+	fi
+
 	mkdir -p $tdata
 	env LIT_ITEST_ROOT=$(realpath $tdata) ./itest_$t.py
 	if [ $? != 0 ]; then
@@ -33,4 +45,4 @@ for t in $tests; do
 	n=$(($n + 1))
 done
 
-echo "All ($n) tests passed."
+echo "All ($n) tests passed.  (Ignored: $ign)"
