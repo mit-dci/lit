fae4de37edd79aec05a511f890c34de9844c6aa9
Trey Del Bonis <j.delbonis.3@gmail.com>
2018-08-08 14:15:50 -0400

    Made the test runner script a lot more robust, using Bash some job control features.

--- test/runtests.sh
+++ test/runtests.sh
@@ -16,9 +16,26 @@ echo '========'
 rm -rf $datadir
 
 n=0
+fail=0
 ign=0
 
+export EXIT_REQED=0
+export TEST_PID=-1
+
+function killcurrentandexit () {
+	echo 'testpid!' $TEST_PID
+	export EXIT_REQED=1
+}
+
+trap killcurrentandexit INT
+
 for t in $tests; do
+
+	if [ "$EXIT_REQED" == "1" ]; then
+		ign=$(($ign + 1))
+		continue
+	fi
+
 	echo "Running test: $t"
 	echo '===='
 
@@ -32,17 +49,30 @@ for t in $tests; do
 	fi
 
 	mkdir -p $tdata
-	env LIT_ITEST_ROOT=$(realpath $tdata) ./itest_$t.py
+	env LIT_ITEST_ROOT=$(realpath $tdata) ./itest_$t.py & export TEST_PID=$!
+	wait $TEST_PID
 	if [ $? != 0 ]; then
 		echo -e "\n====\n"
 		echo "Failed: $t"
-		exit 1
+		echo '---'
+		fail=$(($fail + 1))
+		continue
 	fi
 
 	echo -e "\n===="
 	echo "Compeleted: $t"
 	echo '----'
 	n=$(($n + 1))
+
 done
 
-echo "All ($n) tests passed.  (Ignored: $ign)"
+if [ "$EXIT_REQED" == "1" ]; then
+	kill -2 $TEST_PID
+	echo 'Waiting for tests to exit... (This may take several seconds)'
+	wait $TEST_PID
+fi
+
+echo 'All tests completed.'
+echo "Passed: $n"
+echo "Failed: $fail"
+echo "Ignored: $ign"
