4e1ea25af0542865f9a34b4dfb766b3c51139aa2
Varunram <vrg2009@ymail.com>
2018-09-09 12:09:30 +0530

    rnd

--- test/runtests.sh
+++ test/runtests.sh
@@ -45,7 +45,7 @@ for t in $tests; do
 	echo "PWD"
 	echo $PWD
 	mkdir -p $tdata
-	./$PWD/itest_$t.py & export TEST_PID=$!
+	python3 $PWD/itest_$t.py & export TEST_PID=$!
 	echo "STEP 2"
 	echo $TEST_PID
 	wait $TEST_PID
