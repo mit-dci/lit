d5103dfdcbeb6ab65142f059c1df2609ee618a40
Varunram <vrg2009@ymail.com>
2018-09-09 11:35:08 +0530

    rnd

--- test/itest_testlib.py
+++ test/itest_testlib.py
@@ -5,7 +5,17 @@ import testlib
 create = 2
 
 def run_test(env):
-    litcnt = len(env.lits)
+    length = len(env.lits)
+    print("LENGTH IS", length)
+    alice = env.lits[0]
+    bob = env.lits[1]
+    print('Connecting Alice', alice.lnid, 'to Bob', bob.lnid)
+    alice.connect_to_peer(bob)
+    print('Connected')
+    alice.rpc.Say(Peer=alice.get_peer_id(bob), Message="hello!")
+    print('Alice said hello to Bob.')
+    bob.rpc.Say(Peer=bob.get_peer_id(alice), Message="world!")
+    print('Bob said hello to Alice.')
 
 if __name__ == '__main__':
     env = None
