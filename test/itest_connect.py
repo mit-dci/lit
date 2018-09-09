41e89cb17b9e9cb428f774374a3bcfcb31ce8d93
Varunram <vrg2009@ymail.com>
2018-09-09 11:40:45 +0530

    rnd

--- test/itest_connect.py
+++ test/itest_connect.py
@@ -2,7 +2,11 @@
 
 import testlib
 
+create = 2
+
 def run_test(env):
+    length = len(env.lits)
+    print("LENGTH IS", length)
     alice = env.lits[0]
     bob = env.lits[1]
     print('Connecting Alice', alice.lnid, 'to Bob', bob.lnid)
@@ -16,7 +20,7 @@ def run_test(env):
 if __name__ == '__main__':
     env = None
     try:
-        env = testlib.TestEnv(2)
+        env = testlib.TestEnv(create)
         run_test(env)
     finally:
         if env is not None:
