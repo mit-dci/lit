bf4c478fd944fcefd76f4084d002ba6736eaa90c
Varunram <vrg2009@ymail.com>
2018-09-09 15:19:37 +0530

    rnd

--- test/runtests.sh
+++ test/runtests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash +e
+
+datadir=_data
+
 tests=$(cat tests.txt | grep -vE '^(#|$)' | sed 's/ *#.*//g')
 if [ "$#" -gt 0 ]; then
 	tests=$@
@@ -9,4 +12,19 @@ for t in $tests; do
 	echo "* $t"
 done
 echo '========'
-./itest_testlib.py
+
+rm -rf $datadir
+
+export EXIT_REQED=0
+export TEST_PID=-1
+
+./itest_connect.py && ./itest_break.py
+
+if [ "$EXIT_REQED" == "1" ]; then
+	kill -2 $TEST_PID
+	echo 'Waiting for tests to exit... (This may take several seconds)'
+	wait $TEST_PID
+fi
+
+echo 'All tests completed.'
+echo "Passed: $n"
