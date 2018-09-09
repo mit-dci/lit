147dd3a7fb82c6c2d14a3bbb4de5006cdd3a533c
Varunram <vrg2009@ymail.com>
2018-09-09 15:30:22 +0530

    rnd

--- test/runtests.sh
+++ test/runtests.sh
@@ -15,7 +15,20 @@ export EXIT_REQED=0
 export TEST_PID=-1
 
 ./itest_connect.py
+./itest_receive.py
+./itest_send.py
+./itest_send2.py
+./itest_setgetfee.py
+./itest_fund.py
+./itest_close.py
+./itest_close_reverse.py
 ./itest_break.py
+./itest_break_reverse.py
+./itest_push.py
+./itest_pushclose.py
+./itest_pushclose_reverse.py
+./itest_pushbreak.py
+./itest_pushbreak_reverse.py
 
 if [ "$EXIT_REQED" == "1" ]; then
 	kill -2 $TEST_PID
