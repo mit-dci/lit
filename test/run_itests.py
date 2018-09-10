5ae34b95b8bf5981b6100e7eef992f20e82d7d70
Varunram <vrg2009@ymail.com>
2018-09-10 19:56:54 +0530

    rnd

--- test/run_itests.py
+++ test/run_itests.py
@@ -6,7 +6,21 @@ import sys
 noOfNodes = 2
 
 from itest_connect import run_test as connect
-from itest_receive import run_test as receive
+#from itest_receive import run_test as receive
+from itest_send import run_test as send
+# from itest_send2 import run_test as send2
+# from itest_setgetfee import run_test as setgetfee
+# from itest_fund import run_test as fund
+# from itest_close import run_test as close
+# from itest_close_reverse import run_test as close_reverse
+# from itest_break import run_test as break
+# from itest_break_reverse import run_test as break_reverse
+# from itest_push import run_test as push
+# from itest_pushclose import run_test as pushclose
+# from itest_pushclose_reverse import run_test as pushclose_reverse
+# from itest_pushbreak import run_test as pushbreak
+# from itest_pushbreak_reverse import run_test as pushbreak_reverse
+
 
 if __name__ == '__main__':
     env = None
@@ -18,7 +32,7 @@ if __name__ == '__main__':
             print(e)
         print(env.bitcoind.rpc.getblockchaininfo())
         try:
-            receive(env)
+            send(env)
         except Exception as e:
             print(e)
     finally:
