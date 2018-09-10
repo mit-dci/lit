5ccc1af2b1fae09ee2902197c3e4fdac70184415
Varunram <vrg2009@ymail.com>
2018-09-10 18:58:40 +0530

    rnd

--- test/runtests.sh
+++ test/runtests.sh
@@ -11,18 +11,4 @@ for t in $tests; do
 done
 echo '************************************************'
 
-./itest_connect.py
-./itest_receive.py
-./itest_send.py
-./itest_send2.py
-./itest_setgetfee.py
-./itest_fund.py
-./itest_close.py
-./itest_close_reverse.py
-./itest_break.py
-./itest_break_reverse.py
-./itest_push.py
-./itest_pushclose.py
-./itest_pushclose_reverse.py
-./itest_pushbreak.py
-./itest_pushbreak_reverse.py
+./run_itests.py
