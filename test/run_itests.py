f627ba712cab1a51eab0d0cb56e06507da4308e1
Varunram <vrg2009@ymail.com>
2018-09-10 18:58:40 +0530

    rnd

---
+++ test/run_itests.py
@@ -0,0 +1,17 @@
+import testlib
+import sys
+
+noOfNodes = 2
+
+from itest_connect import run_test as connect()
+
+if __name__ == '__main__':
+    env = None
+    try:
+        env = testlib.TestEnv(noOfNodes)
+        connect(env)
+    finally:
+        if env is not None:
+            #env.shutdown()
+            print("LENGTH of open lits", len(env.lits))
+            #sys.exit(0)
