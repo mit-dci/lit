c6c3577e0da72de1c5a679c28160412168749b18
Varunram <vrg2009@ymail.com>
2018-09-11 13:56:12 +0530

    rnd

--- test/run_itests.py
+++ test/run_itests.py
@@ -9,8 +9,8 @@ from itest_connect import run_test as connect
 from itest_receive import run_test as receive
 from itest_send import run_test as send
 from itest_send2 import run_test as send2
-# from itest_setgetfee import run_test as setgetfee
-# from itest_fund import run_test as fund
+from itest_setgetfee import run_test as setgetfee
+from itest_fund import run_test as fund
 # from itest_close import run_test as close
 # from itest_close_reverse import run_test as close_reverse
 # from itest_break import run_test as break
@@ -40,6 +40,14 @@ if __name__ == '__main__':
             print(e)
         try:
             send2(env)
+        except Exception as e:
+            print(e)
+        try:
+            setgetfee(env)
+        except Exception as e:
+            print(e)
+        try:
+            fund(env)
         except Exception as e:
             print(e)
     finally:
