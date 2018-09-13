2e7a34e9d9004ff459572d9babc634d6ade6d077
Varunram <vrg2009@ymail.com>
2018-09-13 08:34:29 +0530

    rnd

--- test/test_combinators.py
+++ test/test_combinators.py
@@ -64,8 +64,9 @@ def run_pushclose_test(env, initiator, target, closer):
     tt1 = target.get_balance_info()['TxoTotal']
 
     # Now report the difference in channel balance.
-    print('Target:', tt0, '->', tt1, '( expected:', initialsend + pushsend - 20000, ')')
-    assert tt1 == tt0 + initialsend + pushsend - 20000, "final balance doesn't match"
+    # 200 is the fee amount, wish we could have some sort of constatns in the tests for that
+    print('Target:', tt0, '->', tt1, '( expected:', initialsend + pushsend - 200, ')')
+    assert tt1 -tt0 == initialsend + pushsend - 20000, "final balance doesn't match"
 
 def run_pushbreak_test(env, initiator, target, breaker):
     bc = env.bitcoind
