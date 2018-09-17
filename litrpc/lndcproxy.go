dce9aeb3213705fdbbbab1ec824039ef28a65a51
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2018-09-17 15:38:42 +0200

    Code documentation for remote control

--- litrpc/lndcproxy.go
+++ litrpc/lndcproxy.go
@@ -10,10 +10,15 @@ import (
 	"golang.org/x/net/websocket"
 )
 
+// LndcRpcWebsocketProxy is a regular websocket server that translates the
+// received requests into a call on the new LNDC based remote control transport
 type LndcRpcWebsocketProxy struct {
 	lndcRpcClient *LndcRpcClient
 }
 
+// NewLocalLndcRpcWebsocketProxy is an overload to NewLndcRpcWebsocketProxyWithLndc
+// connecting to the local lit using a derived key from the locally stored
+// privkey.hex using the default home dir and port
 func NewLocalLndcRpcWebsocketProxy() (*LndcRpcWebsocketProxy, error) {
 	client, err := NewLocalLndcRpcClient()
 	if err != nil {
@@ -22,6 +27,9 @@ func NewLocalLndcRpcWebsocketProxy() (*LndcRpcWebsocketProxy, error) {
 	return NewLndcRpcWebsocketProxyWithLndc(client), nil
 }
 
+// NewLocalLndcRpcWebsocketProxy is an overload to NewLndcRpcWebsocketProxyWithLndc
+// connecting to the local lit using a derived key from the locally stored
+// privkey.hex using the default home dir and the given port
 func NewLocalLndcRpcWebsocketProxyWithPort(port uint32) (*LndcRpcWebsocketProxy, error) {
 	client, err := NewLocalLndcRpcClientWithPort(port)
 	if err != nil {
@@ -30,6 +38,9 @@ func NewLocalLndcRpcWebsocketProxyWithPort(port uint32) (*LndcRpcWebsocketProxy,
 	return NewLndcRpcWebsocketProxyWithLndc(client), nil
 }
 
+// NewLocalLndcRpcWebsocketProxy is an overload to NewLndcRpcWebsocketProxyWithLndc
+// connecting to the local lit using a derived key from the locally stored
+// privkey.hex using the given homedir and the given port
 func NewLocalLndcRpcWebsocketProxyWithHomeDirAndPort(litHomeDir string, port uint32) (*LndcRpcWebsocketProxy, error) {
 	client, err := NewLocalLndcRpcClientWithHomeDirAndPort(litHomeDir, port)
 	if err != nil {
@@ -38,6 +49,9 @@ func NewLocalLndcRpcWebsocketProxyWithHomeDirAndPort(litHomeDir string, port uin
 	return NewLndcRpcWebsocketProxyWithLndc(client), nil
 }
 
+// NewLndcRpcWebsocketProxy is an overload to NewLndcRpcWebsocketProxyWithLndc
+// connecting to the given lit node specified by litAdr, identifying with the
+// given key.
 func NewLndcRpcWebsocketProxy(litAdr string, key *koblitz.PrivateKey) (*LndcRpcWebsocketProxy, error) {
 	client, err := NewLndcRpcClient(litAdr, key)
 	if err != nil {
@@ -46,12 +60,18 @@ func NewLndcRpcWebsocketProxy(litAdr string, key *koblitz.PrivateKey) (*LndcRpcW
 	return NewLndcRpcWebsocketProxyWithLndc(client), nil
 }
 
+// NewLndcRpcWebsocketProxyWithLndc creates a new proxy that listens on the
+// websocket port and translates requests into remote control messages over
+// lndc transport.
 func NewLndcRpcWebsocketProxyWithLndc(lndcRpcClient *LndcRpcClient) *LndcRpcWebsocketProxy {
 	proxy := new(LndcRpcWebsocketProxy)
 	proxy.lndcRpcClient = lndcRpcClient
 	return proxy
 }
 
+// Listen starts listening on the given host and port for websocket requests
+// This function blocks unless an error occurs, so you should call it as a
+// goroutine
 func (p *LndcRpcWebsocketProxy) Listen(host string, port uint16) {
 
 	listenString := fmt.Sprintf("%s:%d", host, port)
@@ -69,10 +89,14 @@ func (p *LndcRpcWebsocketProxy) Listen(host string, port uint16) {
 	logging.Errorf("Error on websocket server: %s", err.Error())
 }
 
+// proxyServeWS handles incoming websocket requests
 func (p *LndcRpcWebsocketProxy) proxyServeWS(ws *websocket.Conn) {
 
+	// Close the connection when we're done
 	defer ws.Close()
 	for {
+
+		// Receive a websocket frame
 		var data interface{}
 		err := websocket.JSON.Receive(ws, &data)
 		if err != nil {
@@ -84,31 +108,50 @@ func (p *LndcRpcWebsocketProxy) proxyServeWS(ws *websocket.Conn) {
 			break
 		}
 		var reply interface{}
+		// Parse the incoming request as generic JSON
 		msg := data.(map[string]interface{})
 		method := msg["method"]
 		var args interface{}
+
+		// Default to a NoArgs in case the params array is nil
 		args = new(NoArgs)
 
 		if msg["params"] != nil {
+
+			// Parse the params into the args object
 			argsArray, ok := msg["params"].([]interface{})
 			if ok && len(argsArray) > 0 {
+				// In JsonRpc 2.0, the params are always an array
+				// but lit always only expects a single Args object
+				// so we take index 0 from the array.
 				args = msg["params"].([]interface{})[0]
 			}
 		}
+
+		// store the id from the jsonrpc request
 		id := msg["id"]
+
+		// Use the LNDC RPC client to execute the call
 		err = p.lndcRpcClient.Call(method.(string), args, &reply)
 		var jsonResponse []byte
 		if err != nil {
+			// In case of an error send back a JSON RPC formatted error
+			// response
 			jsonResponse, _ = json.Marshal(errorResponse(id, err))
 		} else {
+			// In case of success, send back a JSON RPC formatted
+			// response
 			jsonResponse, _ = json.Marshal(successResponse(id, reply))
 		}
 
+		// Write the response to the stream, and continue with the next websocket
+		// frame
 		ws.Write(jsonResponse)
 	}
 
 }
 
+// baseResponse adds the basic elements for each response type to the JSON Response
 func baseResponse(requestId interface{}) map[string]interface{} {
 	response := make(map[string]interface{})
 	response["jsonrpc"] = "2.0"
@@ -116,6 +159,7 @@ func baseResponse(requestId interface{}) map[string]interface{} {
 	return response
 }
 
+// errorResponse translates an error into a properly formatted JSON RPC response
 func errorResponse(requestId interface{}, err error) map[string]interface{} {
 	errorObj := make(map[string]interface{})
 	errorObj["code"] = -32000
@@ -128,6 +172,7 @@ func errorResponse(requestId interface{}, err error) map[string]interface{} {
 	return response
 }
 
+// successResponse formats a reply object into a properly formatted JSON RPC response
 func successResponse(requestId interface{}, data interface{}) map[string]interface{} {
 	response := baseResponse(requestId)
 	response["result"] = data
