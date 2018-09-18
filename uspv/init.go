02b909ff03415824520dc590d395371b0d53b2c2
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2018-09-18 16:16:59 +0200

    Allow connecting to coindaemon peers by hostname

--- uspv/init.go
+++ uspv/init.go
@@ -27,15 +27,7 @@ func IP4(ipAddress string) bool {
 func (s *SPVCon) parseRemoteNode(remoteNode string) (string, string, error) {
 	colonCount := strings.Count(remoteNode, ":")
 	var conMode string
-	if colonCount == 0 {
-		if IP4(remoteNode) || remoteNode == "localhost" { // need this to connect to locahost
-			remoteNode = remoteNode + ":" + s.Param.DefaultPort
-		}
-		// only ipv4 clears this since ipv6 has colons
-		conMode = "tcp4"
-		return remoteNode, conMode, nil
-	} else if colonCount == 1 && IP4(strings.Split(remoteNode, ":")[0]) {
-		// custom port on ipv4
+	if colonCount <= 1 {
 		return remoteNode, "tcp4", nil
 	} else if colonCount >= 5 {
 		// ipv6 without remote port
