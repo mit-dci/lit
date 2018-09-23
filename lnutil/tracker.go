1cd5fb306e0a4fcffbb8fe2a764a265489d66145
Varunram <vrg2009@ymail.com>
2018-09-23 14:37:45 +0530

    correct bug in tracker connection

--- lnutil/tracker.go
+++ lnutil/tracker.go
@@ -6,13 +6,15 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"errors"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/mit-dci/lit/crypto/koblitz"
 	"github.com/mit-dci/lit/logging"
 	"golang.org/x/net/proxy"
-	"net/http"
-	"net/url"
-	"strings"
-	"time"
 )
 
 type announcement struct {
@@ -36,6 +38,7 @@ func Announce(priv *koblitz.PrivateKey, port int, litadr string, trackerURL stri
 	client := &http.Client{
 		Timeout: time.Second * 4, // 4+4 to accomodate the 10s RPC timeout
 	}
+	strport := ":" + strconv.Itoa(port)
 	resp, err := client.Get("https://ipv4.myexternalip.com/raw")
 	if err != nil {
 		return err
@@ -45,7 +48,7 @@ func Announce(priv *koblitz.PrivateKey, port int, litadr string, trackerURL stri
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(resp.Body)
 
-	liturlIPv4 := strings.TrimSpace(buf.String()) + ":" + string(port)
+	liturlIPv4 := strings.TrimSpace(buf.String()) + strport
 
 	var liturlIPv6 string
 
@@ -59,7 +62,7 @@ func Announce(priv *koblitz.PrivateKey, port int, litadr string, trackerURL stri
 		defer resp.Body.Close()
 		buf = new(bytes.Buffer)
 		buf.ReadFrom(resp.Body)
-		liturlIPv6 = strings.TrimSpace(buf.String()) + ":" + string(port)
+		liturlIPv6 = strings.TrimSpace(buf.String()) + strport
 	}
 
 	urlBytes := []byte(liturlIPv4 + liturlIPv6)
