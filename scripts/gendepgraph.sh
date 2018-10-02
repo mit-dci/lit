140e13df75a4594c0e003f72fad1b9578fb5b28c
delbonis <j.delbonis.3@gmail.com>
2018-10-02 15:03:51 -0400

    Updated the gendepgraph.sh script to work how it is now.

--- scripts/gendepgraph.sh
+++ scripts/gendepgraph.sh
@@ -6,4 +6,5 @@ if [ -n "$1" ]; then
 	out=$1
 fi
 
-godepgraph -s . | sed 's/github.com\/mit-dci\/lit\///' | dot -Tpng -o $1
+source 'build/env.sh'
+godepgraph -s . | sed 's/github.com\/mit-dci\/lit\///' | dot -Tpng -o $out
